0a1c85fc913971ac18530a5244c5d21043d73772
udit.sethi <udit.sethi@concentrix.com>
2024-04-09 12:04:16 +0530

    Learned how to read request body and setImmediate function used to call callbacks asynchronously.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const routesPaths = require("./routes/routes")
 const http = require('http');
+const fs = require('fs');
 const port = 50050;
 const server = http.createServer((req, resp)=>{
     const urlRequested = req.url;
@@ -12,6 +13,24 @@ const server = http.createServer((req, resp)=>{
             break;
 
         case routesPaths.profile:
+            const body = [];
+            req.on('data',(chunk)=>{
+                console.log(chunk);
+                body.push(chunk);
+            })
+            req.on('end',()=>{
+                const parsedBody = Buffer?.concat(body)?.toString();
+                console.log(parsedBody);
+                //fs.writeFileSync('Recieved Json Data From Request',parsedBody); //blocks the execeution of code after till the time file is not created successfully.
+                fs.writeFile('Recieved Json Data From Request 2',parsedBody,err=>{
+                    console.log(err);
+                    if(err)
+                    {
+                        resp.writeHead(302);
+                        return resp.write('There is some error in creating the file');
+                    }
+                });
+            })
             resp.writeHead(200);
             resp.write( `Hey I am running an Profile page because requested url was ${urlRequested}`);
             resp.end();
@@ -23,7 +42,7 @@ const server = http.createServer((req, resp)=>{
                 <title>Form Page</title>
             </head>
             <body>
-                <form method="POST" action="/profile">
+                <form method="POST" action="/createFile">
                     <input type="text" name="randomname" placeholder="Enter Random Name">
                     <button type="submit">Submit</button>
                 </form>
@@ -32,6 +51,28 @@ const server = http.createServer((req, resp)=>{
             resp.writeHead(200);
             resp.write(formElement);
             resp.end();
+            break;
+        
+        case routesPaths.createFile:
+            if(req.method==="POST")
+            {
+                const body = [];
+                req.on('data',(chunk)=>{
+                    body.push(chunk);
+                    console.log(chunk);
+                });
+                req.on('end',()=>{
+                    console.log('body: ', body);
+                    const parsedBody = Buffer.concat(body).toString();
+                    console.log('parsedBody: ',parsedBody);
+                    const message = parsedBody?.split('=')?.[1];
+                    console.log(message);
+                    fs.writeFileSync("New File 2",message);
+                })
+                resp.writeHead(201);
+                resp.write("Filte created successfully");
+                resp.end();
+            }
             break;
     
         default:
@@ -56,3 +97,15 @@ server.listen(port);
     console.log(process.argv);
     process.exit();
 },5000)*/
+
+setTimeout(()=>{
+    console.log("set time out after 10 s");
+},9000)
+
+
+setImmediate(()=>{
+    console.log("set Immediate out after 10 s");
+})
+
+console.log("I am udit sethi");
+console.log("Are u understanding my concernbro?");
--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 module.exports = {
     "home": "/home",
     "profile": "/profile",
-    "form": "/form"
+    "form": "/form",
+    "createFile": "/createFile",
 };
