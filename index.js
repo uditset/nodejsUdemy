8962a4129b637460ab9c59954c1242a6393ddb4e
udit.sethi <udit.sethi@concentrix.com>
2024-04-10 19:48:22 +0530

    express-learning-p1-complete

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-const http = require('http');
+const express = require('express');
 const {handleIncomingRequests} = require('./routes/routes.js');
-const server = http?.createServer(handleIncomingRequests);
 const PORT = 3000;
 
-server?.listen(PORT,()=>console.log(`SERVER STARTED AT PORT ${PORT}`));
+const app = express();
+
+const middleWare1 = (req,res,next) => {
+    console.log("I am in middle ware 1");
+    next();
+}
+
+const middleWare2 = (req,res,next) => {
+    console.log("I am in middle ware 2");
+    next();
+}
+
+const middleWare3 = (req,res,next) => {
+    console.log("I am in middle ware 3");
+    next();
+    //res?.send(`<h1>Welcome to express Js Framework.</h1`)
+}
+
+app.use('/',middleWare1);  
+app.use(middleWare2); //is equivalent to app.use('/',middleWare2);  / here means all request starting from '/'
+app.use(middleWare3);
+
+//one of the way of mapping request.
+app.use('/product',(req,res,next)=>{
+    console.log(req.url);
+    console.log(req.method);
+    console.log(req.body);
+    console.log(req.body);
+    res.send('<h1>Hi bro this is product page</h1>');
+})
+app.use('*',(req,res,next)=>{
+    res?.send('<h1>Page not found bro</h1>');
+})
+
+app?.listen(PORT,()=>console.log(`Express App started at PORT ${PORT}`));
 
 
 //Doubts arised switch case was not working in routes.js in handleIncomingRequests thats why I replaced with if else
